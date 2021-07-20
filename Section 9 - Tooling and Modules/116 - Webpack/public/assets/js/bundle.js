ddead5ccbc6882fe9970a77e2693265ed3cb4dbc
lucastanh <lucasvinicius564@hotmail.com>
2021-07-20 16:37:44 -0300

    updating

---
+++ Section 9 - Tooling and Modules/116 - Webpack/public/assets/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// a
+// a
+// a
+// a
+console.log('aaaaa');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
